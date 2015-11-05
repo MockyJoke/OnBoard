a0c16aa40ceaed125a4a86fd253c8385f1360dfe
mgrmx <lyc16@asb9700-e02.csil.sfu.ca>
2015-11-04 16:13:51 -0800

    Add Start Screen buttons
    + button radius

--- ProjectCode/OnBoard/OnBoard/StartupViewController.swift
+++ ProjectCode/OnBoard/OnBoard/StartupViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class StartupViewController: UIViewController {
 
+    @IBOutlet weak var sessionButton: UIButton!
+    
+    @IBOutlet weak var prevSessionButton: UIButton!
+    
+    @IBOutlet weak var changeProfileButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.sessionButton.layer.cornerRadius = 10
+        self.prevSessionButton.layer.cornerRadius = 10
+        self.changeProfileButton.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
