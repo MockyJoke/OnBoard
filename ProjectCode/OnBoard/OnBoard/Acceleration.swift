a8cf4adf5a6cf101e8c417f206f6f9d1a4834825
George Cui <gcui@hcc-1340-07.ucslabs.sfu.ca>
2015-11-07 12:02:45 -0800

    Working on CMMotion

---
+++ ProjectCode/OnBoard/OnBoard/Acceleration.swift
@@ -0,0 +1,16 @@
+//
+//  Acceleration.swift
+//  OnBoard
+//
+//  Created by George Cui on 11/6/15.
+//  Copyright (c) 2015 Rainbow Riders. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+struct distancePerSecond{
+    internal private(set) var Distance : Double
+    internal private(set) var Time : Double
+    internal private(set) var Acceleration : Double
+}
