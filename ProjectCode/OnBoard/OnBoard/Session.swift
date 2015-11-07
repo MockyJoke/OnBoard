88af16685f46bd5ec66d3bcf72053fde7c0c7fb2
Johnson Pan <johnsonp@hcc-1340-08.ucslabs.sfu.ca>
2015-11-07 12:51:55 -0800

    Add session mgr and related class

---
+++ ProjectCode/OnBoard/OnBoard/Session.swift
@@ -0,0 +1,46 @@
+//
+//  Session.swift
+//  OnBoard
+//
+//  Created by Johnson Pan on 11/7/15.
+//  Copyright (c) 2015 Rainbow Riders. All rights reserved.
+//
+
+import Foundation
+
+class Session : NSObject{
+    internal private(set) var StartTime : NSDate?
+    internal private(set) var EndTime : NSDate?
+    internal private(set) var IsStarted : Bool
+    internal private(set) var IsEnded : Bool
+    
+    override init(){
+        IsStarted = false
+        IsEnded = false
+    }
+    
+    // To start this Session!
+    func StartSession(){
+        if ( !IsStarted && !IsEnded){
+            IsStarted = true
+            StartTime = NSDate()
+        }
+    }
+    
+    // To end this session!
+    func EndSession(){
+        IsEnded = true;
+        EndTime = NSDate();
+    }
+    
+    //
+    func GetDuration() -> NSTimeInterval{
+        if ( IsStarted && IsEnded){
+            return EndTime!.timeIntervalSinceDate(StartTime!)
+        }else if(IsStarted && !IsEnded){
+            return NSDate().timeIntervalSinceDate(StartTime!)
+        }else{
+            return 0;
+        }
+    }
+}
