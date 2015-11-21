151a223e685b8bb72312c2dbb70dc9f595367295
rajdeeep <rajdeepk@wmc-2502-67.ucslabs.sfu.ca>
2015-11-20 19:58:08 -0800

    implementation of session savings

--- ProjectCode/OnBoard/OnBoard/SessionSnapshot.swift
+++ ProjectCode/OnBoard/OnBoard/SessionSnapshot.swift
@@ -8,16 +8,18 @@
 //
 import Foundation
 
-struct SessionSnapshot{
+class SessionSnapshot : StorableObject,NSCoding{
     var snapshotTime : NSDate
     var locationSnapshot : LocationSnapshot?
-    var motionSnapshot : MotionSnapshot
-    init (){
+    //var motionSnapshot : MotionSnapshot?
+    override init (){
         snapshotTime = NSDate()
         if(CoreLocationManager.sharedInstance.IsLocationAvail){
             locationSnapshot = LocationSnapshot(location: CoreLocationManager.sharedInstance.latestLocation!)
         }
-        motionSnapshot = MotionSnapshot (accleration: MotionManager.sharedInstance.GetCurrentAcceleration())
+      //  motionSnapshot = MotionSnapshot (accleration: MotionManager.sharedInstance.GetCurrentAcceleration())
+        super.init()
+        
     }
     
     
@@ -27,7 +29,21 @@ struct SessionSnapshot{
         if(CoreLocationManager.sharedInstance.IsLocationAvail){
             newSnapshot.locationSnapshot = LocationSnapshot(location: CoreLocationManager.sharedInstance.latestLocation!)
         }
-        newSnapshot.motionSnapshot = MotionSnapshot(accleration: MotionManager.sharedInstance.GetCurrentAcceleration())
+      //  newSnapshot.motionSnapshot = MotionSnapshot(accleration: MotionManager.sharedInstance.GetCurrentAcceleration())
         return newSnapshot
     }
+    
+    required init(coder aDecoder: NSCoder) {
+        self.snapshotTime = SessionSnapshot.decodeHelper(coder: aDecoder,propertyName: "snapshotTime",defaultVal: NSDate())
+        self.locationSnapshot = SessionSnapshot.decodeHelper(coder: aDecoder, propertyName: "locationSnapshot", defaultVal: nil )
+        //self.motionSnapshot = SessionSnapshot.decodeHelper(coder: aDecoder, propertyName: "motionSnapshot", defaultVal: nil )
+        super.init()
+    }
+    
+    override func encodeWithCoder(aCoder:NSCoder){
+        aCoder.encodeObject(snapshotTime,forKey:"snapshotTime")
+        aCoder.encodeObject(locationSnapshot,forKey:"locationSnapshot")
+        //aCoder.encodeObject(motionSnapshot,forKey:"motionSnapshot")
+    
+            }
 }
