6c82396f941918c728a959ce265994649695cc0e
Git-Good <gcui@hcc-1340-01.ucslabs.sfu.ca>
2015-11-22 16:56:04 -0800

    Name Pin Feature

--- ProjectCode/OnBoard/OnBoard/MapViewController.swift
+++ ProjectCode/OnBoard/OnBoard/MapViewController.swift
@@ -13,6 +13,9 @@ import MapKit
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tagCurrentLocationButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,20 +26,30 @@ class MapViewController: UIViewController {
         
     }
     
+
+    /*Following Variables for Pin Dropping & Alert feature
+    var inputedText1: UITextField?
+    var inputedText2: UITextField?
     
+    var canceled: Bool
     
+    End of Pin Dropping & Alert Variables
+    */
     /* Zoom in on User, missing "resort" coordinates
     func centerOnUser(){
         var regionRadius : CLLocationDistance = 500
         mapView.setRegion(MKCoordinateRegionMakeWithDistance(resort.GetCoordinate2D(), regionRadius, regionRadius), animated: true)
     }*/
     
+    var inputedText1: String = "Place"
+    var inputedText2: String = "Short Description"
+    var canceled: Bool!
 
     
     @IBAction func addPin(sender: UILongPressGestureRecognizer) {
         
         sender.minimumPressDuration = 2.0
-        
+        mapView.showsUserLocation = false
         showPinAlert()
         
         let location = sender.locationInView(self.mapView)
@@ -46,39 +59,72 @@ class MapViewController: UIViewController {
         
         annotation.coordinate = coordinates
         
-        annotation.title = "Place"
-        annotation.subtitle = "Short Description"
+        annotation.title = inputedText1+"HIT"
+        annotation.subtitle = inputedText2
         
         self.mapView.addAnnotation(annotation)
+        
+        mapView.showsUserLocation = true
+        
         
         //self.mapView.removeAnnotation(<#annotation: MKAnnotation!#>)
         
     }
     
+    
+    
+  
     func showPinAlert() {
-        var alert = UIAlertController(title: "Create Geo-Tag?", message: "Save this location on your Maps", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
-        var name = alert.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+        var alertcontroller = UIAlertController(title: "Create Geo-Tag?", message: "Save this location on your Maps", preferredStyle: UIAlertControllerStyle.Alert)
+
+        
+        alertcontroller.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
             textField.placeholder = "Name this location"
+            
             //textField.secureTextEntry = true
+
         })
             
-        var description = alert.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+        alertcontroller.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
             textField.placeholder = "Describe this location"
+            
             //textField.secureTextEntry = true
             
         })
-            self.presentViewController(alert, animated: true, completion: nil)
+        
+        alertcontroller.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {(action) -> Void in
+            let input1 = alertcontroller.textFields?.first as! UITextField
+            self.inputedText1 = input1.text
+            
+            println(input1.text+"HIHIHI")
+        
+        }))
+            
+        alertcontroller.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: {(action) -> Void in
+        self.canceled = true
+    
+        }))
+            self.presentViewController(alertcontroller, animated: true, completion: nil)
     }
+
     
     
+    @IBAction func tagCurrentLocationButton(sender: AnyObject) {
+        showPinAlert()
+        let annotation = MKPointAnnotation()
+        annotation.title = "Place"
+        
+        annotation.subtitle = "Short Description"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: self.mapView.userLocation.coordinate.latitude, longitude: self.mapView.userLocation.coordinate.longitude)
+        self.mapView.addAnnotation(annotation)
+    }
 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func userLocationButtonAction(sender: AnyObject) {
         CenterMapToUserLocation()
     }
