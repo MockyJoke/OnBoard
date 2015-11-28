1b2ec3e8d371c17160b02a77f5e855364b3cd9fb
Louis <lyc16@hcc-1340-04.ucslabs.sfu.ca>
2015-11-28 15:48:09 -0800

    Add Statistics Detail View Graph/ Seg Control

--- ProjectCode/OnBoard/OnBoard/StatDetailsViewController.swift
+++ ProjectCode/OnBoard/OnBoard/StatDetailsViewController.swift
@@ -17,14 +17,41 @@ class StatDetailsViewController: UIViewController {
     @IBOutlet weak var totalTime: UILabel!
     @IBOutlet weak var totalDistance: UILabel!
     @IBOutlet weak var averageSpeed: UILabel!
-
+    @IBOutlet weak var dataToggleSegControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         totalTime.text = "\(Time)" + " mins"
         totalDistance.text = "\(Distance)" + " m"
         averageSpeed.text = "\(Speed)" + " km/h"
+        
+        let chartArea = CGRect(x: 0, y: 45, width: self.view.bounds.width, height: self.view.bounds.height / 2)
+        
+        let chart = TKChart(frame: CGRectInset(chartArea, 15, 30))
+        chart.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.FlexibleWidth.rawValue | UIViewAutoresizing.FlexibleHeight.rawValue)
+        self.view.addSubview(chart)
+        
+        var randomNumericData = [TKChartDataPoint]()
+        for i in 0..<10 {
+            randomNumericData.append(TKChartDataPoint(x: i, y: Double(arc4random() % 100)))
+        }
+        
+        
+        let chartSeries = TKChartLineSeries(items: randomNumericData)
+        chart.addSeries(chartSeries)
+        chart.xAxis.title = "Time (min)"
+        chart.title().hidden = false
+        chart.title().text = "This is a chart demo"
+        chart.legend().hidden = false
+        
+        
     }
 
+    @IBAction func segControlChanged(sender: AnyObject) {
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
