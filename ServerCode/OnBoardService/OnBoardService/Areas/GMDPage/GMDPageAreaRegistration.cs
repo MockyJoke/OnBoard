d7e4341b96c05ca68653ee2388fdd75341632d56
Johnson Pan <johnsonpan333@gmail.com>
2016-02-12 03:41:53 -0800

    Add tempo GMD Page

---
+++ ServerCode/OnBoardService/OnBoardService/Areas/GMDPage/GMDPageAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace OnBoardService.Areas.GMDPage
+{
+    public class GMDPageAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GMDPage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GMDPage_default",
+                "GMD/{id}",
+                new { controller="GMDPage", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
